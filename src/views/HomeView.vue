23e4e64efedef26f56364317dadab2515ad7d6aa
Tanyeem <tanyeemsafwan123@gmail.com>
2022-12-25 22:16:49 +0600

    first 2 compositions API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,38 @@
 <script setup>
+import { useFlash } from '../composables/useFlash';
 import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from "vue";
+
+let hello = ref("Hello World");
+
+let { flash } = useFlash();
+
+setTimeout(() => {
+  hello.value = "Value changed";
+}, 2000);
+
+// let clicked = () => {
+//   alert("You clicked the button!");
+// };
+let food = ref('');
+
+// let flash = (message) => {
+//   swal(message);
+// };
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>
+      {{ hello }}
+    </p>
+    <p>
+      <input type="text" v-model="hello" />
+    </p>
+    <button @click="flash('Successful!', 'Random message!', 'info')">click me</button>
+    <div>
+      <input type="text" v-model="food" />
+    </div>
   </main>
 </template>
